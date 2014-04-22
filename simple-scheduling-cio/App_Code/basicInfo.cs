b1667d57e669e4a3d549b831b2cb25cc2d337c57
jptremblay <jptremblay3@gmail.com>
2014-04-21 22:11:19 -0400

    Changes to Main Schedule
    Deleted the Duration column and added a column for Birth Date and Med
    Rec Number

--- simple-scheduling-cio/App_Code/basicInfo.cs
+++ simple-scheduling-cio/App_Code/basicInfo.cs
@@ -22,11 +22,13 @@ public class basicInfo{
         info.Columns.Add(dc);
         dc = new DataColumn("Patient");
         info.Columns.Add(dc);
+        dc = new DataColumn("Birth Date");
+        info.Columns.Add(dc);
+        dc = new DataColumn("Med Rec Number");
+        info.Columns.Add(dc);
         dc = new DataColumn("Location");
         info.Columns.Add(dc);
         dc = new DataColumn("Appt Date");
-        info.Columns.Add(dc);
-        dc = new DataColumn("Duration");
         info.Columns.Add(dc);
         dc = new DataColumn("Details");
         info.Columns.Add(dc);
@@ -39,17 +41,18 @@ public class basicInfo{
             
             for (int x = 0; x < InfoDataTable.Columns.Count; x++)
             {
-                if(x<3) row[x] =(InfoDataTable.Rows[i][x].ToString());
-                if (x==3){
+               
+                if(x<5) row[x] =(InfoDataTable.Rows[i][x].ToString());
+                if (x==5)
+                {
                     string dateVal = InfoDataTable.Rows[i][x].ToString();
-                    string time = InfoDataTable.Rows[i][4].ToString();
+                    string time = InfoDataTable.Rows[i][6].ToString();
                     string pattern = "yyyyMMddHHmm";
                     DateTime parsedDate;
                     DateTime.TryParseExact(dateVal+time, pattern, null,System.Globalization.DateTimeStyles.None, out parsedDate);
-                    row[x] = parsedDate.ToString("MM/dd/yyyy-hh:mm tt");
-                    
+                    row[x] = parsedDate.ToString("MM/dd/yyyy-hh:mm tt"); 
                 }
-                if (x >5) row[x-2] = (InfoDataTable.Rows[i][x].ToString());
+                if (x >7) row[x-2] = (InfoDataTable.Rows[i][x].ToString());
             }
             info.Rows.Add(row);
         }
