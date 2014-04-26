8295b0aa23fa49663e3a016e2856c5ccc0b1e4d2
Mathew <mtgeorge@bsu.edu>
2014-04-25 21:30:56 -0400

    Drag Drop requires user logged in
    I made it so the user has to be logged in to be able to change the order
    of surgery page appointments

---
+++ simple-scheduling-cio/Scripts/dragDrop.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    var start; var finish;
+    $('.selectable').sortable({
+        axis: "y",
+        cursor: "move",
+        containment: "parent",
+        tolerance: "pointer",
+        opacity: 0.8,
+        placeholder: "sortable-placeholder",
+
+        start: function (e, ui) {
+            start = ui.item.index() + 1;
+        },
+
+        update: function (e, ui) {
+            finish = ui.item.index() + 1;
+        },
+
+        stop: function (e, ui) {
+            $("#beginVal").val(start);
+            $("#finalVal").val(finish);
+            $("#loginform").submit();
+        }
+    });
+    $('.selectable').disableSelection();
+});
